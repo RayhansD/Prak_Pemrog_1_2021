df419a7ed58f69f0eec54426b1797bc42858166a
RayhansD <rayhansdanendra.2021@student.uny.ac.id>
2021-09-15 15:15:48 +0700

    belajar variabel dalam java

--- Pertemuan 3/CobaVar.java
+++ Pertemuan 3/CobaVar.java
@@ -1,3 +1,10 @@
 public class CobaVar {
-    
+    public static void main(String[] args){
+        String namaDepan = "Rayhans";
+        int umur = 18;
+        double tinggiBadan = 178;
+        System.out.println("Nama saya " + namaDepan);
+        System.out.println("Umur saya " + umur);
+        System.out.println("Tinggi badan saya " + tinggiBadan);
+    }
 }
