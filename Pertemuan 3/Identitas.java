2c4f38b19442de7cb0a6447589e5e10d231e73e4
RayhansD <rayhansdanendra.2021@student.uny.ac.id>
2021-09-15 23:09:23 +0700

    Tugas Praktek Pertemuan 3

---
+++ Pertemuan 3/Identitas.java
@@ -0,0 +1,23 @@
+// Tugas Praktek Pertemuan 3
+public class Identitas {
+    public static void main(String[] args){
+        String namalengkap = "Rayhans Danendra";
+        String namaPanggilan = "Rayhans";
+        String nim = "21537144029";
+        String kelas = "J2";
+        double angkatan = 2021;
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "JPTEI";
+        String fakultas = "Teknik";
+        double ipkHarapan = 3.5;
+        System.out.println("Nama saya " + namalengkap);
+        System.out.println("Biasa dipanggil " + namaPanggilan);
+        System.out.println("NIM saya " + nim);
+        System.out.println("Dari kelas " + kelas);
+        System.out.println("Saya angkatan " + (int) angkatan);
+        System.out.println("Prodi " + programStudi);
+        System.out.println("Jurusan " + jurusan);
+        System.out.println("Fakultas " + fakultas);
+        System.out.println("Target ipk saya adalah " + ipkHarapan);
+    }
+}
---
+++ Pertemuan 3/identitas.py
@@ -0,0 +1,19 @@
+# Tugas Praktek Pertemuan 3
+namaLengkap = "Rayhans Danendra"
+namaPanggilan = "Rayhans"
+nim = 21537144029
+kelas = "J2"
+angkatan = 2021
+programStudi = "Teknologi Informasi"
+jurusan = "JPTEI"
+fakultas = "Teknik"
+ipkHarapan = 3.5
+print("Nama saya " + namaLengkap)
+print("Biasa dipanggil " + namaPanggilan)
+print("NIM saya " + str(nim))
+print("Dari kelas " + kelas)
+print("Saya angkatan " + str(angkatan))
+print("Prodi " + programStudi)
+print("Jurusan " + jurusan)
+print("Fakultas " + fakultas)
+print("Target ipk saya adalah " + str(ipkHarapan))
